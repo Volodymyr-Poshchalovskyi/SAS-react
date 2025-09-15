d62aec85e80edf6d000225ad324c5d7d3bd0cb45
DaniTheHot <volodymyr.poschalovskyi@gmail.com>
2025-09-15 13:17:43 +0300

    updated supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,13 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Отримуємо змінні середовища за допомогою синтаксису Vite
+// З міркувань безпеки, ці значення потрібно зберігати в .env файлі
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Перевіряємо, чи змінні існують, щоб уникнути помилок
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase URL and Anon Key are required.");
-}
-
-// Створюємо та експортуємо клієнт
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
