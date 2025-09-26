48a0d9c9b273b640c1f50534c686d6424d8d72f1
danithehot <volodymyr.poshchalovskyi@gmail.com>
2025-09-26 20:26:40 +0300

    directors data updated

--- src/Data/DirectorsData.js
+++ src/Data/DirectorsData.js
@@ -13,6 +13,11 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'L\'Oréal - Dream or Reality', src: "front-end/01-Directors/01-SUPERNOVA/1. L'Oréal - Dream or Reality.mp4" },
+      { title: 'L\'Oréal - Dream or Reality', src: "front-end/01-Directors/01-SUPERNOVA/2. Levi's - The sound of your soul.mp4" },
+            { title: 'L\'Oréal - Dream or Reality', src: "front-end/01-Directors/01-SUPERNOVA/3. Coca Cola - Real Magic A.mp4" },
+      { title: 'L\'Oréal - Dream or Reality', src: "front-end/01-Directors/01-SUPERNOVA/4. Sloggi - House Party.mp4" },
+
+
     ],
   },
   {
@@ -23,6 +28,11 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'Nike - Hyperboot Launch', src: "front-end/01-Directors/02-CHRISTOPHER SIMS/1-nike_-_hyperboot_launch (1440p).mp4" },
+            { title: 'Nike - Hyperboot Launch', src: "front-end/01-Directors/02-CHRISTOPHER SIMS/2-hyperice_-_naomi_osaka_-_keep_moving (1080p).mp4" },
+                  { title: 'Nike - Hyperboot Launch', src: "front-end/01-Directors/02-CHRISTOPHER SIMS/3-nike_-_hyperboot_teaser (2160p).mp4" },
+                        { title: 'Nike - Hyperboot Launch', src: "front-end/01-Directors/02-CHRISTOPHER SIMS/4-nike_-_jayden_daniels (2160p).mp4" },
+                              { title: 'Nike - Hyperboot Launch', src: "front-end/01-Directors/02-CHRISTOPHER SIMS/5-nike_-_sha_carri_richardson (2160p).mp4" },
+
     ],
   },
   {
@@ -31,7 +41,13 @@ export const directorsData = [
     slug: 'antony-hoffman',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/1-Uae - Gaurdians Of A Nation.mp4" }],
+        videos: [
+          { title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/1-Uae - Gaurdians Of A Nation.mp4" },
+          { title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/2-Toyota Highlander 'Home Team'.mp4" },
+          { title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/3-Puegeot 'Language Of Attraction'.mp4" },
+{ title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/4-Nyu Langone 'One Beat'.mp4" },
+{ title: 'UAE - Guardians of a Nation', src: "front-end/01-Directors/03-ANTONY HOFFMAN/5-Audi E Tron 'I Am Electrcity'.mp4" }
+        ]
   },
   {
     id: 4,
@@ -41,6 +57,13 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/1-zegnaaaaaa.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/2-Mattia Benetti 2-Ud-2025-Anss-Hero-16X9-30-Textless.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/3-Atlantis - The Royal Dubai.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/4 - BULGARI VIPER_HD_DOWNLOAD.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/5 - Sfera Ebbasta & J Balvin - Baby_FHD_DOWNLOAD.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/6- SPLASH Ramadam_FHD_DOWNLOAD.mp4" },
+      { title: 'Zegna', src: "front-end/01-Directors/04-MATTIA BENETTI/7-Mattia Benetti S1155 Starbucks Equity Dc V04 (1).mp4" },
+
     ],
   },
   {
@@ -49,7 +72,13 @@ export const directorsData = [
     slug: 'eli-sverdlov',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/1-Skyworth - W81 Pro - See The Wonder.mp4" }],
+    videos: [{ title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/1-Skyworth - W81 Pro - See The Wonder.mp4" },
+      { title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/2 - Jim Beam - A Look Inside.mp4" },
+      { title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/3 - Huawei - App Gallery.mp4" },
+      { title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/4 - Huawei - P30.mp4" },
+      { title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/5 - Mi Max - Slide To The Future.mp4" },
+      { title: 'Skyworth - See The Wonder', src: "front-end/01-Directors/05-ELI SVERDLOV/6 - Pico - Imagination.mp4" },
+    ],
   },
   {
     id: 6,
@@ -59,6 +88,11 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/1 - Smirnoff Karol G - Poco Pico.mp4" },
+      { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/2 - Bad Bunny - Ni Bien Ni Mal [Official Muaic Video].mp4" },
+      { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/3 - Jennifer Lopez - Pa' Ti Feat Maluma.mp4" },
+      { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/4 - Cadillac LYRIX - The Arrival Ft Goyo.mp4" },
+      { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/5 - Bravas ¦ Tráiler Oficial (1080p_24fps_H264-128kbit_AAC).mp4" },
+      { title: 'Smirnoff Karol G', src: "front-end/01-Directors/06-jessy terrero/6 - Jennifer Lopez, Rauw Alejandro - Cambia el Paso (Official Video) (1080p_24fps_H264-128kbit_AAC).mp4" },
     ],
   },
   {
@@ -67,7 +101,12 @@ export const directorsData = [
     slug: 'vivienne-and-tamas',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'Kerastase - Emily Ratajkowski', src: "front-end/01-Directors/07-VIVIENNE AND TAMAS/01. Kerastase - Chroma- Emily Ratajkowski.mp4" }],
+    videos: [
+      { title: 'Kerastase - Emily Ratajkowski', src: "front-end/01-Directors/07-VIVIENNE AND TAMAS/01. Kerastase - Chroma- Emily Ratajkowski.mp4" },
+      { title: 'Kerastase - Emily Ratajkowski', src: "front-end/01-Directors/07-VIVIENNE AND TAMAS/02. Douglas Cosmetics - Eyes.mp4" },
+      { title: 'Kerastase - Emily Ratajkowski', src: "front-end/01-Directors/07-VIVIENNE AND TAMAS/03. Darphin - Joy Journey.mp4" },
+      { title: 'Kerastase - Emily Ratajkowski', src: "front-end/01-Directors/07-VIVIENNE AND TAMAS/04. Wella Professionals – Illumina Color.mp4" },
+    ],
   },
   {
     id: 8,
@@ -77,6 +116,10 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'Q225 Summer', src: "front-end/01-Directors/08-LORENZO CISI/1 - Q225_SUMMER_LONGFORM_60s_16x9_v13b_ONLINE.mp4" },
+      { title: 'Q225 Summer', src: "front-end/01-Directors/08-LORENZO CISI/2 - LORENZO CISI - ZARA KIDS - SS25 Studio Collection.mp4" },
+      { title: 'Q225 Summer', src: "front-end/01-Directors/08-LORENZO CISI/3 - givenchy_-_l'interdit_thrill.mp4" },
+      { title: 'Q225 Summer', src: "front-end/01-Directors/08-LORENZO CISI/4 - LORENZO CISI - Zara Kids - SS24 - The Artists.mp4" },
+      { title: 'Q225 Summer', src: "front-end/01-Directors/08-LORENZO CISI/5 - ysl_-_beauty_club_video_booth_feat._sink_the_pink.mp4" },
     ],
   },
   {
@@ -85,7 +128,14 @@ export const directorsData = [
     slug: 'remy-cayuela',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/1 - Mcdonalds - Raise Your Eyebrows.mp4" }],
+    videos: [
+      { title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/1 - Mcdonalds - Raise Your Eyebrows.mp4" },
+            { title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/2 - Travelzoo - The Seal.mp4" },
+      { title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/3 - Vanarama.mp4" },
+      { title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/4 - Match.com - Valentine's Day.mp4" },
+      { title: 'McDonalds - Raise Your Eyebrows', src: "front-end/01-Directors/09-REMY CAYUELA/5-Front-End 01-Directors 09-Remy Cayuela 5 - Younited - Wedding.mp4" },
+
+    ],
   },
   {
     id: 10,
@@ -95,7 +145,9 @@ export const directorsData = [
     bio: 'Lorem ipsum dolor sit amet...',
     videos: [
       { title: 'SA 2025 Launch', src: "front-end/01-Directors/10-Beedy/1-DD_SB_60_DunKings_Sometimes_YouTube.mp4" },
-      
+      { title: 'SA 2025 Launch', src: "front-end/01-Directors/10-Beedy/2-Front-End 01-Directors 10-Beedy Ausr4494h Sa 2025 Launch Davidanddave Streaming 60(1).mp4" },
+      { title: 'SA 2025 Launch', src: "front-end/01-Directors/10-Beedy/3-Front-End 01-Directors 10-Beedy Gift Ratings 60 Directors 12.5 Prores.mp4" },
+      { title: 'SA 2025 Launch', src: "front-end/01-Directors/10-Beedy/4-The-Dunkalatte----She-Loves-That-Coffee-Milk----ft-Kristen-Wiig.mp4" },
     ],
   },
   {
@@ -104,7 +156,15 @@ export const directorsData = [
     slug: 'victor-heinz',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/1 - viktor_heinz__karl_kani__pattern_of_the_street_-_director (1080p).mp4" }],
+    videos: [
+      { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/1 - viktor_heinz__karl_kani__pattern_of_the_street_-_director (1080p).mp4" },
+            { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/2 - viktor_heinz___pepsi__eintracht_(dc)_-_director (1080p).mp4" },
+      { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/3 - viktor_heinz___asphaltgold__nike_-cortez_(dc_subs)_-_director (1080p).mp4" },
+      { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/4 - Asphalt Gold x Adidas-Superstar_UHD_4K_DOWNLOAD.mp4" },
+      { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/5 - viktor_heinz___hennessy_-_director (1080p).mp4" },
+      { title: 'Karl Kani - Pattern of the Street', src: "front-end/01-Directors/11-VICTOR HEINZ/6 - viktor_heinz___kaufland__crazywolf_(dc)_-_director (1080p).mp4" },
+
+    ],
   },
   {
     id: 12,
@@ -112,6 +172,14 @@ export const directorsData = [
     slug: 'jean-claude-thibaut',
     photoSrc: placeholderPhoto,
     bio: 'Lorem ipsum dolor sit amet...',
-    videos: [{ title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/1 - Kilian - Old Fashioned.mp4" }],
+    videos: [
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/1 - Kilian - Old Fashioned.mp4" },
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/2 - Estée Lauder Dare_UHD_4K_DOWNLOAD.mp4" },
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/3 - S9 - GIFTED_UHD_DOWNLOAD.mp4" },
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/4 - LOUIS VUITTON ¦ NEVERFULL_FHD_DOWNLOAD.mp4" },
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/5 - Emidio Tucci - El Cortes Ingles X Jon Hamm.mp4" },
+      { title: 'Kilian - Old Fashioned', src: "front-end/01-Directors/12-JEAN CLAUDE THIBAUT/6 - BLENDERS PRIDE - PRIYNKA CHOPRA_HD_DOWNLOAD.mp4" },
+
+    ],
   },
 ];
